821607c062d92b38159d4239d7d997c11b2d4dd8
Leczó Gábor Bálint <leczo.gabor@proton.me>
2025-08-09 14:41:27 +0200

    Install Laravel Horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AuroraPanelProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
